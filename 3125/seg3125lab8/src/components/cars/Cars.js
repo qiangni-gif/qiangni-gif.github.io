ebd9c3470647ee208adc8a3b7ca51f5d12f3ab22
qiangni <34262202+qiangni@users.noreply.github.com>
2020-07-12 01:05:34 -0400

    Create Cars.js

---
+++ 3125/seg3125lab8/src/components/cars/Cars.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import CarItem from './CarItem';
+import {connect} from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import SimpleSelect from "./SimpleSelect";
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& > *': {
+		margin: theme.spacing(1),
+		width: '30ch',
+		},
+		formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+		},
+		selectEmpty: {
+		marginTop: theme.spacing(2),
+		},
+	},
+}));
+
+
+
+
+
+class Cars extends Component {
+
+	
+
+	render() {
+		return (
+			<div className="container pt-5">
+				<h4>Cars For Sale</h4>
+				<hr/>
+				<div>
+					<Grid
+					container
+					spacing={2}
+					direction="row"
+					justify="flex-start"
+					alignItems="stretch"
+					>
+						<Grid item xs>
+							<div class = "card mb-3 shadow bg-light">
+								<form className={useStyles.root} noValidate autoComplete="off">
+									<TextField id="standard-basic" label="Search" fullWidth />
+								</form>
+								<SimpleSelect/>
+							</div>
+						</Grid>
+
+						<Grid item xs={9}>
+							<Grid container direction="column" justify="flex-start" alignItems="stretch">
+							<Grid item xs>
+							<div>
+								{this.props.carsArray.map(eachCar=>{
+									return (
+										<CarItem key={eachCar.id} eachCar={eachCar} />
+									)
+								})}
+							
+							</div>
+							</Grid>
+							</Grid>
+						</Grid>
+
+					</Grid>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+const mapStateToProps = state => {
+	return { carsArray: [...state.cars] }
+}
+
+
+
+export default connect(mapStateToProps, null)(Cars);
